0be3a9c05a5bf6a8091b38fa36c08a07ca93f002
Yashasvi Vij <yashasvivij01@gmail.com>
2026-09-03 12:18:44 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for(var i = 0; i<nums.length-1; i++){
+            for(var j = i+1;j<nums.length;j++){
+if(nums[i]+nums[j] === target){
+    return [i,j]
+}else{
+    continue;
+}
+            }
+        }
+    }
+}
